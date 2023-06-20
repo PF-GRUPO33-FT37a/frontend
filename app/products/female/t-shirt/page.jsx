5a23f69219626aee3775f38a6a600c59ca1a5bb1
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-20 01:40:06 -0300

    redux completo

--- app/products/female/t-shirt/page.jsx
+++ app/products/female/t-shirt/page.jsx
@@ -3,12 +3,15 @@ import ContainerProducts from "@/components/ContainerProducts"
 import {useState, useEffect} from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { getProducts } from "@/redux/Slice"
+import FilterBar from "@/components/FilterBar"
 
 export default function FemaleShirtPage() {
 
     const dispatch = useDispatch();
     const allProducts = useSelector((state) => state.products.allProducts);
+    const renderProducts = useSelector((state)=>state.products.filterProducts)
     const [products, setProducts] = useState([]);
+    const [render, setRender] = useState([])
   
     useEffect(() => {
       dispatch(getProducts("female", "shirt"));
@@ -19,134 +22,23 @@ export default function FemaleShirtPage() {
           setProducts(allProducts);
         }
       }, [allProducts]);
-
-    const [rangeValue, setRangeValue] = useState(0)
-
-    const handleRange = (event) =>{
-        setRangeValue(event.target.value)
-    }
-
     
+      useEffect(() => {
+        if (renderProducts && renderProducts.length > 0) {
+            console.log({RENDER:renderProducts})
+          setRender(renderProducts);
+        }
+      }, [renderProducts]);
+
 
     return (
         <main className="pt-[9rem] min-h-[100vh]">
             <section className="w-[70%] mx-[auto] flex py-[3rem]">
-                <div className="w-[20%]">
-
-
-                    <article className="w-[100%]  p-[0.6rem] flex flex-col gap-y-[1rem]">
-                        <h3 className="border-[#A9A9B2] border-b-[1px]">Marca</h3>
-                        <input
-                            className="w-[100%] text-[0.8rem] p-[0.4rem] border-[#A9A9B2] border-[1px]"
-                            type="text" placeholder="Buscar por marca" />
-
-                        <div
-                            className="flex flex-col h-[120px] overflow-y-scroll">
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Adidas</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Nike</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Reebok</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Puma</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Vans</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">NBA</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Wilson</span>
-                            </label>
-                        </div>
-                        <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]">Aplicar</h4>
-
-                    </article>
-
-                    <article className="w-[100%] p-[0.6rem] flex flex-col gap-y-[1rem]">
-                        <h3 className="border-[#A9A9B2] border-b-[1px]">Talle</h3>
-                        <input
-                            className="w-[100%] text-[0.8rem] p-[0.4rem] border-[#A9A9B2] border-[1px]"
-                            type="text" placeholder="Buscar por talle" />
-
-                        <div
-                            className="flex flex-col h-[120px] overflow-y-scroll">
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">37</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">38</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">39</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">40</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">41</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">42</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">43</span>
-                            </label>
-                        </div>
-                        <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]">Aplicar</h4>
-
-                    </article>
-
-                    <article className="w-[100%] p-[0.6rem] flex flex-col gap-y-[1rem]">
-                        <h3 className="border-[#A9A9B2] border-b-[1px]">Color</h3>
-                        <input
-                            className="w-[100%] text-[0.8rem] p-[0.4rem] border-[#A9A9B2] border-[1px]"
-                            type="text" placeholder="Buscar por color" />
-
-                        <div
-                            className="flex flex-col h-[120px] overflow-y-scroll">
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Blanco</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Negro</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Rojo</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Azul</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Verde</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Violeta</span>
-                            </label>
-                            <label htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">Marron</span>
-                            </label>
-                        </div>
-                        <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]">Aplicar</h4>
-
-                    </article>
-
-                    <article className="w-[100%] p-[0.6rem] flex flex-col gap-y-[1rem]">
-                    <h3 className="border-[#A9A9B2] border-b-[1px]">Precio</h3>
-                        <input value={rangeValue} onChange={handleRange} type="range" min={0} max={10000} />
-                        <span>{rangeValue}</span>
-                        <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]">Aplicar</h4>
-
-                    </article>
-                </div>
+                
+                {products&&products.length>0?<FilterBar products={products} gender={"female"} category={"shirt"}/>:<p>loading...</p>}
 
                 <div className="w-[80%]">
-                    {products&&products.length>1?<ContainerProducts products={products}/>:<p>loading...</p>}
+                    {render&&render.length>0?<ContainerProducts products={render}/>:<p>loading...</p>}
                 </div>
             </section>
         </main>
--- components/FilterBar.jsx
+++ components/FilterBar.jsx
@@ -1,19 +1,33 @@
 'use client'
 import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { getFilterProducts } from "@/redux/Slice"
 
-export default function FilterBar({ products }) {
+export default function FilterBar({ products, gender, category }) {
 
     const [rangeValue, setRangeValue] = useState(0)
-
+    const [genders, setGenders] = useState("")
+    const [categorys, setCategorys] = useState("")
     const [filterBrands, setFilterBrands] = useState([])
     const [filterColors, setFilterColors] = useState([])
 
-    const [filterBar, setFilterBar] = useState([])
+    const [filterBar, setFilterBar] = useState({
+        brand:"",
+        color:""
+    })
+    const dispatch = useDispatch()
 
-    const handleRange = (event) => {
+    console.log({FILTRADOPRINCIPAL:genders+" "+categorys})
+
+    useEffect(()=>{setGenders(gender)},[gender])
+    useEffect(()=>{setCategorys(category)},[category])
+
+    useEffect(()=>{dispatch(getFilterProducts(gender,category))},[genders,categorys])
+    
+    const handleRange = (event) =>{
         setRangeValue(event.target.value)
     }
-
+    
     const getBrands = () => {
         const arrayBrands = products?.flatMap((el) => el.brand[0])
         const brands = new Set(arrayBrands)
@@ -33,9 +47,36 @@ export default function FilterBar({ products }) {
         console.log(value);
         console.log(checked);
         if(checked){
-            setFilterBar([...filterBar,value])
+            if(filterBar.brand==""){
+                let valor = filterBar.brand+value+","
+                setFilterBar({...filterBar,brand:valor})
+            }
+            else{
+                let valor = filterBar.brand+value+","
+                setFilterBar({...filterBar,brand:valor})
+            }
         }else{
-            setFilterBar(filterBar.filter((brand => brand !== value)))
+            let newBrands = filterBar.brand.replace(new RegExp(value + ",?"), "");
+            setFilterBar({...filterBar, brand:newBrands})
+        }
+    }
+
+    const handleColorChange = (event) =>{
+        const {value, checked} = event.target;
+        console.log(value);
+        console.log(checked);
+        if(checked){
+            if(filterBar.color==""){
+                let valor = filterBar.color+value+","
+                setFilterBar({...filterBar,color:valor})
+            }
+            else{
+                let valor = filterBar.color+value+","
+                setFilterBar({...filterBar,color:valor})
+            }
+        }else{
+            let newColors = filterBar.color.replace(new RegExp(value + ",?"), "");
+            setFilterBar({...filterBar, color:newColors})
         }
     }
     
@@ -48,6 +89,18 @@ export default function FilterBar({ products }) {
     console.log(filterBrands);
 
     console.log(filterBar);
+
+
+    const handleSubmit = (event) =>{
+        if ((filterBar.brand && filterBar.brand.length>1)||(filterBar.color && filterBar.color.length>1)){
+            let brand = filterBar.brand
+            let color = filterBar.color
+            dispatch(getFilterProducts(genders, categorys, brand!=","?brand:null, color!=","?color:null))
+        }
+        else{
+            dispatch(getFilterProducts(genders, categorys))
+        }
+    }
 
     return (
         <div className="w-[20%]">
@@ -74,7 +127,7 @@ export default function FilterBar({ products }) {
                         })
                     }
                 </div>
-                <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]">Apply</h4>
+                <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]" onClick={handleSubmit}>Apply</h4>
 
             </article>
 
@@ -123,13 +176,13 @@ export default function FilterBar({ products }) {
                         filterColors?.map((filter, index) => {
                             return (
                             <label key={index} htmlFor="">
-                                <input type="checkbox" /> <span className="text-[#A9A9B2]">{filter}</span>
+                                <input type="checkbox" value={filter} onChange={handleColorChange}/> <span className="text-[#A9A9B2]">{filter}</span>
                             </label>
                             )
                         })
                     }
                 </div>
-                <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]">Apply</h4>
+                <h4 className="bg-[white] text-[#A9A9B2] text-[0.8rem] p-[0.4rem] text-center cursor-pointer border-[#A9A9B2] border-[1px]" onClick={handleSubmit}>Apply</h4>
             </article>
 
             <article className="w-[100%] p-[0.6rem] flex flex-col gap-y-[1rem]">
