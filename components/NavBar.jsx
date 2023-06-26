4e8ab4b4b9fe26bb4f2e1e737ee9222c7ea50e2d
anfevera <116735342+anfevera97@users.noreply.github.com>
2023-06-25 23:04:40 -0500

    auth local storage fix

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useState, useEffect, useCallback } from 'react';
 import Image from 'next/image';
 import logo from '../public/logocommerce.png';
@@ -10,74 +10,95 @@ import { useRouter, usePathname } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 import { debounce } from 'lodash';
 import { searchProducts, clearState } from '@/redux/Slice';
+import { useSession } from 'next-auth/react';
+import axios from 'axios';
 
 export default function NavBar() {
-    const [search, setSearch] = useState('');
-    const router = useRouter()
-    const dispatch = useDispatch()
-    const userData = localStorage.getItem('user')
+	const [search, setSearch] = useState('');
+	const router = useRouter();
+	const dispatch = useDispatch();
+	const userData = localStorage.getItem('user');
+	const { data: session } = useSession();
 
-    useEffect(() => {
-        const myCartLocal = localStorage.getItem('myCart')
-        if (!myCartLocal) {
-            localStorage.setItem("myCart", JSON.stringify([]))
-        }
-    }, [])
-    
-    const pathname = usePathname() 
-    useEffect(() => {
-        if(pathname.includes("products")){
-        setSearch("")}
-  },[pathname])
+	useEffect(() => {
+		const myCartLocal = localStorage.getItem('myCart');
+		if (!myCartLocal) {
+			localStorage.setItem('myCart', JSON.stringify([]));
+		}
+	}, []);
 
-    const debouncedSearch = useCallback(
-        debounce((searchTerm) => {
-            if (searchTerm.length > 0) {
-                router.push('/search')
-                dispatch(searchProducts(searchTerm));
-            }
-        }, 500),
-        []
-    );
-  
-    const handleChange = (event) => {
-        const searchTerm = event.target.value;
-        setSearch(searchTerm);
-        if(pathname.includes(search)){
-        debouncedSearch(searchTerm);}
-    };
+	useEffect(async () => {
+		if (session) {
+			const email = session.user.email;
+			const response = await axios.get(
+				`http://localhost:3001/users/auth/${email}`,
+			);
+			console.log(response);
+			localStorage.setItem(
+				'user',
+				JSON.stringify({
+					data: response.data,
+					validated: false,
+				}),
+			);
+		}
+	}, []);
 
-    useEffect(() => {
-        if ((!search||search=="")&&pathname.includes('search')) {
-            dispatch(searchProducts());
-        }
-    }, [search, dispatch]);
+	const pathname = usePathname();
+	useEffect(() => {
+		if (pathname.includes('products')) {
+			setSearch('');
+		}
+	}, [pathname]);
 
-    return (
-        <nav className="flex flex-col justify-between pt-[1rem]  gap-y-[1rem] fixed w-full z-50 bg-white border-b-2 border-black">
-            <div className="flex justify-around w-full items-center">
-                <Link href={'/'}>
-                    <Image src={logo} alt="logo-img" width={80} height={80} />
-                </Link>
-                <input className="bg-[#90909050] w-[45%] p-[0.6rem] rounded-[1rem]  pl-[1rem]" type="text"
-                    onChange={handleChange}
-                    value={search}
-                />
-                <div className="flex items-center gap-x-[2rem]">
+	const debouncedSearch = useCallback(
+		debounce((searchTerm) => {
+			if (searchTerm.length > 0) {
+				router.push('/search');
+				dispatch(searchProducts(searchTerm));
+			}
+		}, 500),
+		[],
+	);
 
-                    {userData ?
-                        <UserMenu />
-                        :
-                        <Link href={'/login'}>Register/Login</Link>
-                    }
-                    <Link href={'/checkout'}>
-                        <Image src={cart} alt="ico-cart" width={40} height={40} />
-                    </Link>
-                </div>
-            </div>
-            <Menu />
-            </nav>
-            )
-        }
+	const handleChange = (event) => {
+		const searchTerm = event.target.value;
+		setSearch(searchTerm);
+		if (pathname.includes(search)) {
+			debouncedSearch(searchTerm);
+		}
+	};
 
-            
+	useEffect(() => {
+		if ((!search || search == '') && pathname.includes('search')) {
+			dispatch(searchProducts());
+		}
+	}, [search, dispatch]);
+
+	return (
+		<nav className='flex flex-col justify-between pt-[1rem]  gap-y-[1rem] fixed w-full z-50 bg-white border-b-2 border-black'>
+			<div className='flex justify-around w-full items-center'>
+				<Link href={'/'}>
+					<Image src={logo} alt='logo-img' width={80} height={80} />
+				</Link>
+				<input
+					className='bg-[#90909050] w-[45%] p-[0.6rem] rounded-[1rem]  pl-[1rem]'
+					type='text'
+					onChange={handleChange}
+					value={search}
+				/>
+				<div className='flex items-center gap-x-[2rem]'>
+					{userData ? (
+						<UserMenu />
+					) : (
+						<Link href={'/login'}>Register/Login</Link>
+					)}
+					<Link href={'/checkout'}>
+						<Image src={cart} alt='ico-cart' width={40} height={40} />
+					</Link>
+				</div>
+			</div>
+			<Menu />
+		</nav>
+	);
+}
