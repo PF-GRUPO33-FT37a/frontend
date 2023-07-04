450cbd3d80632ac895845e9b39bebe9af342b4ed
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-07-04 01:49:19 -0300

    manejo de tokens de seguridad aplicado sobre ususarios y formulario de creacion

--- app/autenticate/page.jsx
+++ app/autenticate/page.jsx
@@ -34,7 +34,12 @@ export default function Autenticate(){
 
     useEffect(()=>{
         if(user && user.isActive){
-            axios.put(`http://localhost:3001/users/${user._id}`,{validated:true})
+            let token = user.token
+            axios.put(`http://localhost:3001/users/${user._id}`,{validated:true}, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                  }
+            })
             .then((response)=>{
                 localStorage.setItem('user',JSON.stringify({
                     data:response.data,
